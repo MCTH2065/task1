76753287ce8b52e190385bb844ba77beffee2c52
Matt <cor_n@mail.ru>
2021-11-25 18:09:27 +0300

    isdone = True

--- main.py
+++ main.py
@@ -1,44 +1,15 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QApplication
-from PyQt5.QtGui import QPainter, QColor
 import sys
 from random import randint
 
-
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(230, 510, 351, 31))
-        font = QtGui.QFont()
-        font.setPointSize(13)
-        self.pushButton.setFont(font)
-        self.pushButton.setObjectName("pushButton")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton.setText(_translate("MainWindow", "Создать жёлтую окружность"))
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QMainWindow, QApplication
 
 
-class circles(Ui_MainWindow, QMainWindow):
+class circles(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
+        uic.loadUi('UI.ui', self)
         self.pushButton.clicked.connect(self.do_draw)
         self.draw = False
 
@@ -58,9 +29,9 @@ class circles(Ui_MainWindow, QMainWindow):
         self.draw = True
         self.update()
 
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     pr = circles()
     pr.show()
     sys.exit(app.exec_())
-
